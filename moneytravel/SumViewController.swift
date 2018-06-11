11c83f5accb2ebc1ad02d55acae5802aab6793c2
net7ton <net7ton@gmail.com>
2018-06-11 13:00:53 +0300

    editing spends and categories

---
+++ moneytravel/SumViewController.swift
@@ -0,0 +1,39 @@
+//
+//  MoneyViewController.swift
+//  moneytravel
+//
+//  Created by Aleksandr Kharkov on 09/06/2018.
+//  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
+//
+
+import UIKit
+
+class MoneyViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var currencyLabel: UILabel!
+
+    public var onSumEntered: ((Float) -> Void)?
+    private var initSum: Float = 0.0
+    private var initCurrency = "USD"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .plain, target: self, action: #selector(saveSum))
+        textField.becomeFirstResponder()
+        textField.keyboardType = .decimalPad
+    }
+
+    public func setup(caption: String, sum: Float, currency: String) {
+        navigationItem.title = caption
+        initSum = sum
+        initCurrency = currency
+    }
+
+    @objc func saveSum() {
+        navigationController?.popViewController(animated: true)
+
+        let fstr = textField.text ?? ""
+        onSumEntered?(Float(fstr) ?? 0.0)
+    }
+}
