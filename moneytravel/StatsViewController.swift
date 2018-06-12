9d52a39a27b7ba30a8b2897c84b831fc1e463e6e
net7ton <net7ton@gmail.com>
2018-06-12 12:09:52 +0300

    stab for stats screen

---
+++ moneytravel/StatsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  StatsViewController.swift
+//  moneytravel
+//
+//  Created by Aleksandr Kharkov on 12/06/2018.
+//  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
+//
+
+import UIKit
+
+class StatsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
