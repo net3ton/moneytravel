032cef648f1e6a51b6c63ac1e6f6837bb4cdc6b9
net7ton <net7ton@gmail.com>
2018-06-20 00:20:28 +0300

    nothing

--- moneytravel/Consts.swift
+++ moneytravel/Consts.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-let COLOR_KEYS = UIColor(displayP3Red: 0.753, green: 0.792, blue: 0.678, alpha: 1.0)
-let COLOR_KEYS_SELECT = UIColor(displayP3Red: 0.808, green: 0.753, blue: 0.459, alpha: 1.0)
+let COLOR_KEYS = UIColor(red: 0.753, green: 0.792, blue: 0.678, alpha: 1.0)
+let COLOR_KEYS_SELECT = UIColor(red: 0.808, green: 0.753, blue: 0.459, alpha: 1.0)
 
-let COLOR_CAT = UIColor(displayP3Red:0.949, green:0.957, blue:0.933, alpha: 1.0)
-let COLOR_CAT_SELECT = UIColor(displayP3Red:0.886, green:0.906, blue:0.855, alpha: 1.0)
+let COLOR_CAT = UIColor(red:0.949, green:0.957, blue:0.933, alpha: 1.0)
+let COLOR_CAT_SELECT = UIColor(red:0.886, green:0.906, blue:0.855, alpha: 1.0)
 
-let COLOR_SUM_FIELD = UIColor(displayP3Red: 0.804, green:1, blue:0.894, alpha:1.0)
+let COLOR_SUM_FIELD = UIColor(red: 0.804, green:1, blue:0.894, alpha:1.0)
 
-let COLOR_SPEND1 = UIColor(displayP3Red:0.902, green:1, blue:0.945, alpha: 1.0)
+let COLOR_SPEND1 = UIColor(red:0.902, green:1, blue:0.945, alpha: 1.0)
 let COLOR_SPEND2 = UIColor.white
-let COLOR_SPEND_HEADER = UIColor(displayP3Red: 0.714, green:0.976, blue:0.788, alpha:1.0)
+let COLOR_SPEND_HEADER = UIColor(red: 0.714, green:0.976, blue:0.788, alpha:1.0)
 let COLOR_SPEND_FOOTER = UIColor.white
