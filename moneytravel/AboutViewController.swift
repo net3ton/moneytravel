80c1b808d158ed5b2e7d36c1a95dfe67ef0ea25b
net7ton <net7ton@gmail.com>
2018-07-19 17:44:52 +0300

    added about menu with donate buttons removed stats menu (stats will be in history menu)

---
+++ moneytravel/AboutViewController.swift
@@ -0,0 +1,130 @@
+//
+//  AboutViewController.swift
+//  moneytravel
+//
+//  Created by Aleksandr Kharkov on 17/07/2018.
+//  Copyright © 2018 Oleksandr Kharkov. All rights reserved.
+//
+
+import UIKit
+import StoreKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var buttonDonate1: DonateButton!
+    @IBOutlet weak var buttonDonate2: DonateButton!
+    @IBOutlet weak var buttonDonate3: DonateButton!
+    @IBOutlet weak var buttonRate: DonateButton!
+    @IBOutlet weak var textAbout: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        buttonDonate1.price = "1.00 USD"
+        buttonDonate2.price = "5.00 USD"
+        buttonDonate3.price = "20.00 USD"
+
+        textAbout.translatesAutoresizingMaskIntoConstraints = true
+        textAbout.sizeToFit()
+        textAbout.isScrollEnabled = false
+    }
+
+    @IBAction func onRating(_ sender: DonateButton) {
+        if #available(iOS 10.3, *){
+            SKStoreReviewController.requestReview()
+        }
+        else {
+            rateApp(appId: "ID")
+        }
+    }
+}
+
+
+fileprivate func rateApp(appId: String) {
+    openUrl("itms-apps://itunes.apple.com/app/" + appId)
+}
+
+fileprivate func openUrl(_ urlString: String) {
+    let url = URL(string: urlString)!
+    if #available(iOS 10.0, *) {
+        UIApplication.shared.open(url, options: [:], completionHandler: nil)
+    } else {
+        UIApplication.shared.openURL(url)
+    }
+}
+
+
+@IBDesignable class DonateButton: UIButton {
+    private var tapState: Bool = false
+    private var priceLabel: UILabel?
+
+    public var price: String? {
+        set {
+            priceLabel?.text = newValue
+        }
+        get {
+            return priceLabel?.text
+        }
+    }
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initButton()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initButton()
+    }
+    
+    func initButton() {
+        self.layer.cornerRadius = 5
+
+        priceLabel = UILabel()
+        addSubview(priceLabel!)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        if let imgView = self.imageView {
+            imgView.frame = CGRect(x: 8, y: self.bounds.origin.y + 8, width: self.bounds.height - 16, height: self.bounds.height - 16)
+        }
+        
+        if let txtLabel = self.titleLabel {
+            txtLabel.frame = CGRect(x: 50, y: self.bounds.origin.y, width: self.bounds.width / 2, height: self.bounds.height)
+            txtLabel.textAlignment = .left
+        }
+
+        if let priceLabel = self.priceLabel {
+            priceLabel.frame = CGRect(x: self.bounds.width - 108, y: self.bounds.origin.y, width: 100, height: self.bounds.height)
+            priceLabel.textAlignment = .right
+        }
+    }
+
+    private func processTap(pressed: Bool) {
+        let alpha: CGFloat = pressed ? 0.4 : 1.0
+        tapState = pressed
+
+        self.imageView?.alpha = alpha
+        self.titleLabel?.alpha = alpha
+        self.priceLabel?.alpha = alpha
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        processTap(pressed: true)
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesMoved(touches, with: event)
+        let inView = self.bounds.contains(touches.first!.location(in: self))
+        if !inView {
+            processTap(pressed: false)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        processTap(pressed: false)
+    }
+}
