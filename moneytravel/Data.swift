a28691b965b260aaa5013f30e40763b22cf808e6
net7ton <net7ton@gmail.com>
2018-07-19 17:51:10 +0300

    Codable protocol implementation for all entities Logic in AppData for exporting data to JSON or binary formats

--- moneytravel/Data.swift
+++ moneytravel/Data.swift
@@ -72,6 +72,52 @@ private func uicolor_to_int32(_ val: UIColor) -> Int32 {
 }
 
 
+extension CodingUserInfoKey {
+    static let context = CodingUserInfoKey(rawValue: "context")!
+}
+
+
+@objc(CategoryModel)
+public class CategoryModel: NSManagedObject, Codable {
+    public override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case iconname = "icon"
+        case colorvalue = "color"
+        case position = "pos"
+        case removed = "rem"
+    }
+
+    // Decodable
+    public required init(from decoder: Decoder) throws {
+        guard let context = decoder.userInfo[.context] as? NSManagedObjectContext else { fatalError() }
+        guard let entity = NSEntityDescription.entity(forEntityName: "Category", in: context) else { fatalError() }
+        super.init(entity: entity, insertInto: nil)
+
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.name = try values.decode(String.self, forKey: .name)
+        self.iconname = try values.decode(String.self, forKey: .iconname)
+        self.colorvalue = try values.decode(Int32.self, forKey: .colorvalue)
+        self.position = try values.decode(Int16.self, forKey: .position)
+        self.removed = try values.decode(Bool.self, forKey: .removed)
+    }
+
+    // Encodable
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.iconname, forKey: .iconname)
+        try container.encode(self.colorvalue, forKey: .colorvalue)
+        try container.encode(self.position, forKey: .position)
+        try container.encode(self.removed, forKey: .removed)
+    }
+}
+
 extension CategoryModel {
     var icon: UIImage? {
         return UIImage(named: iconname!)
@@ -87,7 +133,46 @@ extension CategoryModel {
     }
 }
 
-extension MarkModel {    
+
+@objc(MarkModel)
+public class MarkModel: NSManagedObject, Codable {
+    public override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case name
+        case date
+        case colorvalue = "color"
+        case removed = "rem"
+    }
+
+    // Decodable
+    public required init(from decoder: Decoder) throws {
+        guard let context = decoder.userInfo[.context] as? NSManagedObjectContext else { fatalError() }
+        guard let entity = NSEntityDescription.entity(forEntityName: "Mark", in: context) else { fatalError() }
+        super.init(entity: entity, insertInto: nil)
+
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.name = try values.decode(String.self, forKey: .name)
+        self.date = try values.decode(Date.self, forKey: .date)
+        self.colorvalue = try values.decode(Int32.self, forKey: .colorvalue)
+        self.removed = try values.decode(Bool.self, forKey: .removed)
+    }
+
+    // Encodable
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.name, forKey: .name)
+        try container.encode(self.date, forKey: .date)
+        try container.encode(self.colorvalue, forKey: .colorvalue)
+        try container.encode(self.removed, forKey: .removed)
+    }
+}
+
+extension MarkModel {
     var color: UIColor {
         get {
             return int32_to_uicolor(colorvalue)
@@ -99,6 +184,58 @@ extension MarkModel {
 }
 
 
+@objc(SpendModel)
+public class SpendModel: NSManagedObject, Codable {
+    public override init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case date
+        case category = "cat"
+        case sum
+        case currency = "iso"
+        case bsum
+        case bcurrency = "biso"
+        case comment = "comm"
+        case removed = "rem"
+    }
+
+    // Decodable
+    public required init(from decoder: Decoder) throws {
+        guard let context = decoder.userInfo[.context] as? NSManagedObjectContext else { fatalError() }
+        guard let entity = NSEntityDescription.entity(forEntityName: "Spend", in: context) else { fatalError() }
+        super.init(entity: entity, insertInto: nil)
+
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.date = try values.decode(Date.self, forKey: .date)
+        self.sum = try values.decode(Float.self, forKey: .sum)
+        self.currency = try values.decode(String.self, forKey: .currency)
+        self.bsum = try values.decode(Float.self, forKey: .bsum)
+        self.bcurrency = try values.decode(String.self, forKey: .bcurrency)
+        self.comment = try values.decode(String.self, forKey: .comment)
+        self.removed = try values.decode(Bool.self, forKey: .removed)
+        
+        let catPosition = try values.decode(Int16.self, forKey: .category)
+        self.category = appCategories.getByPosition(catPosition)
+    }
+
+    // Encodable
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(self.date, forKey: .date)
+        try container.encode(self.category?.position, forKey: .category)
+        try container.encode(self.sum, forKey: .sum)
+        try container.encode(self.currency, forKey: .currency)
+        try container.encode(self.bsum, forKey: .bsum)
+        try container.encode(self.bcurrency, forKey: .bcurrency)
+        try container.encode(self.comment, forKey: .comment)
+        try container.encode(self.removed, forKey: .removed)
+    }
+}
+
 extension SpendModel {
     public func getSumString() -> String {
         return sum_to_string(sum: sum, currency: currency!)
@@ -109,3 +246,119 @@ extension SpendModel {
     }
 }
 
+
+class AppData: Codable {
+    private var baseVer: Int32 = 0
+    private var baseId: String = ""
+
+    private var categories: [CategoryModel] = []
+    private var timestamps: [MarkModel] = []
+    private var spends: [SpendModel] = []
+
+    init() {
+        initData()
+    }
+
+    private func reset() {
+        baseVer = 1
+        baseId = UUID().uuidString
+    }
+
+    public func initData() {
+        let context = get_context()
+        let infoEntity = NSEntityDescription.entity(forEntityName: "Info", in: context)
+        let fetchRequest = NSFetchRequest<InfoModel>(entityName: "Info")
+
+        do {
+            let infos = try context.fetch(fetchRequest)
+            if infos.isEmpty {
+                let info = InfoModel(entity: infoEntity!, insertInto: context)
+                
+                reset()
+                info.id = baseId
+                info.version = baseVer
+                try context.save()
+            }
+            else {
+                baseId = infos[0].id!
+                baseVer = infos[0].version
+            }
+        }
+        catch let error {
+            print("Failed to init data! ERROR: " + error.localizedDescription)
+        }
+    }
+
+    public func fetchAllData() {
+        categories = appCategories.fetchAll()
+        timestamps = appTimestamps.fetchAll()
+        spends = appSpends.fetchAll()
+    }
+
+    public func fetchHistory(_ history: [DaySpends]) {
+        for daily in history {
+            spends.append(contentsOf: daily.spends)
+
+            for spend in daily.spends {
+                if let cat = spend.category, !categories.contains(cat) {
+                    categories.append(cat)
+                }
+            }
+        }
+    }
+
+    static public func load(from data: Data) -> AppData? {
+        let plistDecoder = PropertyListDecoder()
+        plistDecoder.userInfo[.context] = get_context()
+
+        do {
+            return try plistDecoder.decode(AppData.self, from: data)
+        }
+        catch let error {
+            print("Failed to import! ERROR: " + error.localizedDescription)
+        }
+
+        return nil
+    }
+
+    static public func getExportName() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd_HH-mm-ss"
+        return formatter.string(from: Date())
+    }
+
+    public func exportToData(name: String) {
+        do {
+            let plistEncoder = PropertyListEncoder()
+            plistEncoder.outputFormat = .binary
+
+            let plistData = try plistEncoder.encode(self)
+
+            let docsPath = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+            let exportPath = docsPath.appendingPathComponent(String(format: "%@.data", name))
+
+            try plistData.write(to: exportPath)
+        }
+        catch let error {
+            print("Failed to export! ERROR: " + error.localizedDescription)
+        }
+    }
+
+    public func exportToJSON(name: String) {
+        do {
+            let jsonEncoder = JSONEncoder()
+            jsonEncoder.dateEncodingStrategy = .iso8601
+            jsonEncoder.outputFormatting = .prettyPrinted
+
+            let jsonData = try jsonEncoder.encode(self)
+            
+            let docsPath = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+            let exportPath = docsPath.appendingPathComponent(String(format: "%@.json", name))
+            
+            try jsonData.write(to: exportPath)
+        }
+        catch let error {
+            print("Failed to export! ERROR: " + error.localizedDescription)
+        }
+    }
+}
