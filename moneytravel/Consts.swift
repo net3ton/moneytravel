2b5c3647c58a43b2116f75d52a975b0ccfdcee46
net7ton <net7ton@gmail.com>
2018-08-06 17:01:51 +0300

    disabled color for MoneyInput keyboard

--- moneytravel/Consts.swift
+++ moneytravel/Consts.swift
@@ -10,6 +10,7 @@ import UIKit
 
 let COLOR_KEYS = UIColor(red: 0.753, green: 0.792, blue: 0.678, alpha: 1.0)
 let COLOR_KEYS_SELECT = UIColor(red: 0.808, green: 0.753, blue: 0.459, alpha: 1.0)
+let COLOR_KEYS_DISABLED = UIColor(red: 0.600, green: 0.650, blue: 0.500, alpha: 1.0)
 
 let COLOR_CAT = UIColor(red: 0.949, green: 0.957, blue: 0.933, alpha: 1.0)
 let COLOR_CAT_SELECT = UIColor(red: 0.886, green: 0.906, blue: 0.855, alpha: 1.0)
