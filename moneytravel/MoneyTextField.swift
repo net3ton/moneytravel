f980433594d22dd9933396417365c5a41626c0cd
net7ton <net7ton@gmail.com>
2018-09-04 18:33:42 +0300

    showing current currency in the enter sum field

--- moneytravel/MoneyTextField.swift
+++ moneytravel/MoneyTextField.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class MoneyTextField: UITextField {
     var mulLabel: UILabel = UILabel()
+    var currencyLabel: UILabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -22,16 +23,34 @@ class MoneyTextField: UITextField {
     }
 
     private func initLabel() {
-        mulLabel.font = UIFont.systemFont(ofSize: 16)
+        mulLabel.font = UIFont.systemFont(ofSize: 14)
         mulLabel.textColor = UIColor(red:0.20, green:0.20, blue:0.20, alpha:0.30)
         mulLabel.textAlignment = .right
-        
         self.addSubview(mulLabel)
+
+        currencyLabel.font = UIFont.systemFont(ofSize: 14)
+        currencyLabel.textColor = UIColor(red:0.20, green:0.20, blue:0.20, alpha:0.30)
+        currencyLabel.textAlignment = .right
+        self.addSubview(currencyLabel)
+        
+        addTarget(self, action: #selector(textChanged), for: .editingChanged)
+    }
+    
+    @objc func textChanged() {
+        layoutLabels()
+    }
+    
+    private func layoutLabels() {
+        let currentText = text != nil ? text! : ""
+        let offset = self.bounds.width - (currentText.isEmpty ? 106 : 130)
+
+        mulLabel.frame = CGRect(x: offset, y: 4, width: 100, height: 14)
+        currencyLabel.frame = CGRect(x: offset, y: self.bounds.height - 18, width: 100, height: 14)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        mulLabel.frame = CGRect(x: self.bounds.width - 130, y: 0, width: 100, height: self.bounds.height)
+        layoutLabels()
     }
 
     public var multilier: String? {
@@ -40,6 +59,15 @@ class MoneyTextField: UITextField {
         }
         get {
             return mulLabel.text
+        }
+    }
+
+    public var currency: String? {
+        set {
+            currencyLabel.text = newValue
+        }
+        get {
+            return currencyLabel.text
         }
     }
 }
