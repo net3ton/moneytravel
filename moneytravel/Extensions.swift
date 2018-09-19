6531514932b18db856333c2227a1fa05ad0bd410
net7ton <net7ton@gmail.com>
2018-09-20 00:07:02 +0300

    added .temp to data save location

--- moneytravel/Extensions.swift
+++ moneytravel/Extensions.swift
@@ -11,21 +11,43 @@ import Foundation
 public enum ESaveLocation {
     case documents
     case icloud
+    case temp
+}
+
+public func get_shared_path() -> URL? {
+    return FileManager().urls(for: .documentDirectory, in: .userDomainMask).first
+}
+
+public func get_temp_path() -> URL? {
+    return FileManager().temporaryDirectory
 }
 
 extension Data {
-    private func getSharedPath() -> URL? {
-        return FileManager().urls(for: .documentDirectory, in: .userDomainMask).first
+    private func getPath(_ location: ESaveLocation) -> URL? {
+        switch location {
+        case .icloud:
+            return appICloudDrive.getDocumentsPath()
+        case .documents:
+            return get_shared_path()
+        case .temp:
+            return get_temp_path()
+        }
+        
+        //return nil
     }
     
     public func saveTo(_ location: ESaveLocation, withName name: String) -> Bool {
-        guard let path = (location == .icloud) ? appICloudDrive.getDocumentsPath() : getSharedPath() else {
+        guard let path = getPath(location) else {
             return false
         }
         
+        let exportPath = path.appendingPathComponent(name)
+        return saveTo(exportPath)
+    }
+    
+    public func saveTo(_ path: URL) -> Bool {
         do {
-            let exportPath = path.appendingPathComponent(name)
-            try write(to: exportPath)
+            try write(to: path)
         }
         catch let error {
             print("Failed to save data! ERROR: " + error.localizedDescription)
